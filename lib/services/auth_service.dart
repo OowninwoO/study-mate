512b56c7edc913c583f294f1f51dc09af028d72e
niw0906 <niw0906@naver.com>
2026-03-17 00:11:32 +0900

    feat: auth_service 로그인/로그아웃 추가

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  static final GoogleSignIn _googleSignIn = GoogleSignIn.instance;
+  static final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  static User? get currentUser => _firebaseAuth.currentUser;
+
+  static Future<String?> get idToken async {
+    if (currentUser == null) return null;
+    return currentUser!.getIdToken();
+  }
+
+  static Future<void> signInWithGoogle() async {
+    await _googleSignIn.initialize();
+    final googleUser = await _googleSignIn.authenticate();
+    final googleAuth = googleUser.authentication;
+    final credential = GoogleAuthProvider.credential(
+      idToken: googleAuth.idToken,
+    );
+
+    await _firebaseAuth.signInWithCredential(credential);
+  }
+
+  static Future<void> signOut() async {
+    if (currentUser == null) return;
+    if (currentUser!.providerData.isEmpty) return;
+
+    final providerId = currentUser!.providerData.first.providerId;
+
+    await _firebaseAuth.signOut();
+
+    switch (providerId) {
+      case 'google.com':
+        await _googleSignIn.signOut();
+        return;
+      default:
+        return;
+    }
+  }
+}
