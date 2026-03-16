31b23af9767653e4fd94b9444c4d686b5a75fcc3
niw0906 <niw0906@naver.com>
2026-03-16 23:26:42 +0900

    proj: 파이어베이스 프로젝트 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:study_mate/app_router.dart';
+import 'package:study_mate/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MainApp());
 }
 
