1bd7b11f26d8d735ad3e54722f56ab9a2b24eb74
niw0906 <niw0906@naver.com>
2026-03-25 23:00:08 +0900

    api: main_dio_client

---
+++ lib/api/main/main_dio_client.dart
@@ -0,0 +1,46 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:study_mate/services/auth_service.dart';
+
+class MainDioClient {
+  static final Dio dio =
+      Dio(
+          BaseOptions(
+            baseUrl: dotenv.env['MAIN_API_BASE_URL']!,
+            connectTimeout: const Duration(seconds: 10),
+            receiveTimeout: const Duration(seconds: 10),
+            sendTimeout: const Duration(seconds: 10),
+            headers: {'Content-Type': 'application/json'},
+          ),
+        )
+        ..interceptors.add(
+          InterceptorsWrapper(
+            onRequest: (options, handler) async {
+              final token = await AuthService.idToken;
+              if (token != null) {
+                options.headers['idToken'] = token;
+              }
+
+              final caller = options.extra['caller'] ?? 'unknown';
+              print(
+                '[MAIN][REQ][$caller] ${options.method} ${options.baseUrl}${options.path}',
+              );
+
+              handler.next(options);
+            },
+            onResponse: (response, handler) {
+              final caller =
+                  response.requestOptions.extra['caller'] ?? 'unknown';
+              print('[MAIN][RES][$caller] ${response.data}');
+
+              handler.next(response);
+            },
+            onError: (e, handler) {
+              final caller = e.requestOptions.extra['caller'] ?? 'unknown';
+              print('[MAIN][ERR][$caller] ${e.response?.data ?? e.message}');
+
+              handler.next(e);
+            },
+          ),
+        );
+}
