31ec03e5fa3fb42a387020b2b7a325d0e85fa427
niw0906 <niw0906@naver.com>
2026-03-25 22:37:15 +0900

    proj: 환경변수 파일 main에서 로드

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:study_mate/app_router.dart';
 import 'package:study_mate/firebase_options.dart';
 import 'package:study_mate/theme/app_theme.dart';
@@ -7,6 +8,7 @@ import 'package:study_mate/theme/app_theme.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName: '.env');
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MainApp());
