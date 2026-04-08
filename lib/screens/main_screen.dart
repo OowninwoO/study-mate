f4d534aab22df67c7c1a59d4efd37cc3d0fd744e
niw0906 <niw0906@naver.com>
2026-04-08 22:17:41 +0900

    chore: 퀴즈생성중일때 공통앱바 멘트 변경

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:study_mate/providers/quiz/quiz_generating_provider.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends ConsumerWidget {
   final StatefulNavigationShell navigationShell;
 
   const MainScreen({super.key, required this.navigationShell});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isGenerating = ref.watch(quizGeneratingProvider);
+
     return Scaffold(
-      appBar: AppBar(title: const Text('StudyMate'), centerTitle: true),
+      appBar: AppBar(
+        title: Text(isGenerating ? '퀴즈 생성 중' : 'StudyMate'),
+        centerTitle: true,
+      ),
       body: navigationShell,
       bottomNavigationBar: NavigationBar(
         selectedIndex: navigationShell.currentIndex,
