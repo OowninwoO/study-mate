1b9f122b1a8d248fbd65e4ee153b37a45839f45f
niw0906 <niw0906@naver.com>
2026-04-08 22:58:27 +0900

    proj: 앱 공용 앱바/배경색 색상 변경

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -4,8 +4,8 @@ class AppColors {
   static const Color primary = Color(0xFF6C63FF);
   static const Color secondary = Color(0xFF4288F7);
 
-  static const Color appBarBackground = Color(0xFFFCFDFF);
-  static const Color scaffoldBackground = Color(0xFFF7F9FE);
+  static const Color appBarBackground = Color(0xFFE8EAED);
+  static const Color scaffoldBackground = Color(0xFFF3F4F6);
   static const Color bottomNavBackground = Color(0xFFFFFFFF);
 
   static const Color divider = Color(0xFFE3EAF8);
