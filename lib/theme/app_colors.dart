cc1c77ef44d3cfb9f6cbcd49dd5c6f7e0f72a81c
niw0906 <niw0906@naver.com>
2026-04-18 23:56:04 +0900

    proj: 앱 공용색상 추가

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -9,4 +9,10 @@ class AppColors {
   static const Color bottomNavBackground = Color(0xFFFFFFFF);
 
   static const Color divider = Color(0xFFE3EAF8);
+
+  static const Color correct = Color(0xFF2FBF71);
+  static const Color correctBackground = Color(0xFFEAF8F0);
+
+  static const Color wrong = Color(0xFFE85D75);
+  static const Color wrongBackground = Color(0xFFFFEEF1);
 }
